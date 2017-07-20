3a10576a02bed7546eab2202dcfcd7f6ee552797
Ajay Kumar <ajay.kumar@tothenew.com>
2017-07-20 18:35:57 +0530

    1) Added support for making swagger ui version configurable 2) Added support for UTF-8 encoding

--- swagger/grails-app/controllers/swagger/ApiDocController.groovy
+++ swagger/grails-app/controllers/swagger/ApiDocController.groovy
@@ -1,6 +1,9 @@
 package swagger
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.core.io.Resource
 import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 
 class ApiDocController {
 
@@ -9,8 +12,11 @@ class ApiDocController {
     static allowedMethods = [getDocuments: ["GET"]]
     SwaggerService swaggerService
 
+    @Value("classpath*:**/webjars/swagger-ui/**/index.html")
+    Resource[] swaggerUiResources
+
     def getDocuments() {
-        if (request.getHeader('accept') && request.getHeader('accept').indexOf('application/json') > -1) {
+        if (request.getHeader('accept') && request.getHeader('accept').indexOf(MediaType.APPLICATION_JSON_VALUE) > -1) {
             String swaggerJson = "Some error occurred."
             try {
                 swaggerJson = swaggerService.generateSwaggerDocument()
@@ -18,10 +24,18 @@ class ApiDocController {
                 e.printStackTrace();
                 response.status = HttpStatus.INTERNAL_SERVER_ERROR.value()
             }
-            response.contentType = 'application/json'
+            response.contentType = MediaType.APPLICATION_JSON_UTF8_VALUE
             render swaggerJson
         } else {
-            redirect uri: "/webjars/swagger-ui/2.2.5/index.html?url=${request.getRequestURI()}"
+            redirect uri: "/webjars/swagger-ui${getSwaggerUiFile()}?url=${request.getRequestURI()}"
+        }
+    }
+
+    String getSwaggerUiFile() {
+        try {
+            (swaggerUiResources.getAt(0) as Resource).getURI().toString().split("/webjars/swagger-ui")[1]
+        } catch (Exception e) {
+            throw new Exception("Unable to find swagger ui.. Please make sure that you have added swagger ui dependency eg:-\n compile 'org.webjars:swagger-ui:2.2.8' \nin your build.gradle file", e)
         }
     }
 }
