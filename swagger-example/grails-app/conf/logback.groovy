64116661fae54f25003f053dc34b07554eba4955
Bin Le <bin.le@construction.com>
2017-11-01 11:25:58 -0400

    update grails 3.2 framework artifacts

--- swagger-example/grails-app/conf/logback.groovy
+++ swagger-example/grails-app/conf/logback.groovy
@@ -1,17 +1,29 @@
 import grails.util.BuildSettings
 import grails.util.Environment
+import org.springframework.boot.logging.logback.ColorConverter
+import org.springframework.boot.logging.logback.WhitespaceThrowableProxyConverter
+
+import java.nio.charset.Charset
+
+conversionRule 'clr', ColorConverter
+conversionRule 'wex', WhitespaceThrowableProxyConverter
 
 // See http://logback.qos.ch/manual/groovy.html for details on configuration
 appender('STDOUT', ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
-        pattern = "%level %logger - %msg%n"
+        charset = Charset.forName('UTF-8')
+
+        pattern =
+                '%clr(%d{yyyy-MM-dd HH:mm:ss.SSS}){faint} ' + // Date
+                        '%clr(%5p) ' + // Log level
+                        '%clr(---){faint} %clr([%15.15t]){faint} ' + // Thread
+                        '%clr(%-40.40logger{39}){cyan} %clr(:){faint} ' + // Logger
+                        '%m%n%wex' // Message
     }
 }
 
-root(INFO, ['STDOUT'])
-
 def targetDir = BuildSettings.TARGET_DIR
-if (Environment.isDevelopmentMode() && targetDir) {
+if (Environment.isDevelopmentMode() && targetDir != null) {
     appender("FULL_STACKTRACE", FileAppender) {
         file = "${targetDir}/stacktrace.log"
         append = true
@@ -20,24 +32,8 @@ if (Environment.isDevelopmentMode() && targetDir) {
         }
     }
     logger("StackTrace", ERROR, ['FULL_STACKTRACE'], false)
+    root(ERROR, ['STDOUT', 'FULL_STACKTRACE'])
 }
-
-loggerList = ['STDOUT']
-
-logger('grails.app.controllers', DEBUG, loggerList, false)
-logger('grails.app.services', DEBUG, loggerList, false)
-logger('grails.app.jobs', DEBUG, loggerList, false)
-logger('grails.app.domain', DEBUG, loggerList, false)
-logger('grails.app.taglibs', DEBUG, loggerList, false)
-
-logger('grails.app.init.swagger.example.BootStrap', DEBUG, loggerList, false)
-logger('swagger.example.BootStrap', DEBUG, loggerList, false)
-//logger('grails.app.controllers.com.construction.de.prp.audit.AuditInterceptor', TRACE, loggerList, false)
-
-// hibernate SQL and bind variable tracking
-logger('org.hibernate.SQL', DEBUG, ['STDOUT', 'ROLLING_SQL'], false)    // show sql statements
-logger('org.hibernate.type.descriptor.sql.BasicBinder', TRACE, ['ROLLING_SQL'], false)
-// show sql bind variable values
-
-logger('org.apache.http.headers', INFO, loggerList, false)
-//    logger('org.apache.http.wire', DEBUG, loggerList, false)
+else {
+    root(ERROR, ['STDOUT'])
+}
